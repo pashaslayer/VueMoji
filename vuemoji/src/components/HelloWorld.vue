e3676848891608fe2edfa0944575ef30c49dc94e
pashaslayer <pavel.khakhlou@gmx.at>
2024-03-05 15:00:25 +0100

    new changes

--- vuemoji/src/components/HelloWorld.vue
+++ vuemoji/src/components/HelloWorld.vue
@@ -1,4 +1,62 @@
 <template>
+    <div class="container mt-5">
+      <div class="row justify-content-center">
+      <div
+        class="col-md-2 bg-info bg-opacity-25 border border-2 border-white rounded-start border-end align-items-center d-flex justify-content-center"
+      >
+        <label for="username" class="text-center">Username:</label>
+      </div>
+      <div
+        class="col-md-2 m-0 p-0 bg-white border border-2 border-white border-start-0 rounded-end"
+      >
+        <input
+          v-model="postData.username"
+          type="text"
+          class="form-control"
+          id="username-label"
+          @input="validateUsername"
+        />
+      </div>
+    </div>
+
+    <div class="row justify-content-center">
+      <div
+        class="col-md-2 bg-info bg-opacity-25 border border-2 border-top-0 border-white rounded-start border-end align-items-center d-flex justify-content-center"
+      >
+        <label for="password" class="text-center">Password: </label>
+      </div>
+      <div
+        class="col-md-2 m-0 p-0 bg-white border border-2 border-white border-top-0 border-start-0 rounded-end"
+      >
+        <input
+          v-model="postData.password"
+          type="password"
+          class="form-control"
+          id="password-label"
+          aria-describedby="passwordHelp"
+          required
+          @input="validatePassword"
+        />
+      </div>
+    </div>
+
+    <div class="row">
+      <div
+        class="col-12 col-md-6 d-flex justify-content-md-end justify-content-center mb-2"
+      >
+        <button type="submit" class="btn btn-primary" @click="postLogin">
+          Bestätigen
+        </button>
+      </div>
+      <div
+        class="col-12 col-md-6 d-flex justify-content-md-start justify-content-center mb-2"
+      >
+        <button type="submit" class="btn btn-secondary">Abbrechen</button>
+      </div>
+    </div>
+  </div>
+
+
   <div class="container">
     <div v-for="(designGroup, part) in designGroups" :key="part">
       <h3>Wähle {{ part }}-Design:</h3>
@@ -10,40 +68,51 @@
       <p>{{ designGroup.designs[designGroup.currentIndex].image }}</p>
     </div>
   </div>
+  <!-- Duplicated images outside of the loop -->
+  <div class="duplicated-images">
+    <div class="images" v-for="(designGroup, part) in designGroups" :key="'duplicated-' + part">
+      <img :src="require('@/assets/' + designGroup.designs[designGroup.currentIndex].image)" />
+    </div>
+  </div>
 </template>
+
 
 <script>
 export default {
   name: 'vue-moji',
   data() {
     return {
+      postData: {
+        username: "",
+        password: "",
+      },
       designGroups: {
         head: {
           currentIndex: 0,
           designs: [
-            { name: 'Kopf 1', image: 'kopf1.png' },
-            { name: 'Kopf 2', image: 'kopf1.png' },
+            { name: 'Kopf 1', image: 'hair1.png' },
+            { name: 'Kopf 2', image: 'hair2.png' },
           ]
         },
         hair: {
           currentIndex: 0,
           designs: [
-            { name: 'Haar 1', image: 'kopf1.png' },
-            { name: 'Haar 2', image: 'kopf1.png' },
+            { name: 'Haar 1', image: 'head1.png' },
+            { name: 'Haar 2', image: 'head2.png' },
           ]
         },
         body: {
           currentIndex: 0,
           designs: [
-            { name: 'Body 1', image: 'kopf1.png' },
-            { name: 'Body 2', image: 'kopf1.png' },
+            { name: 'Body 1', image: 'body1.png' },
+            { name: 'Body 2', image: 'body2.png' },
           ]
         },
         feet: {
           currentIndex: 0,
           designs: [
-            { name: 'Fuß 1', image: 'kopf1.png' },
-            { name: 'Fuß 2', image: 'kopf1.png' },
+            { name: 'Fuß 1', image: 'feet1.png' },
+            { name: 'Fuß 2', image: 'feet2.png' },
           ]
         }
       }
@@ -68,23 +137,33 @@ export default {
 <style scoped>
 .container {
   display: flex;
-  flex-direction: column; /* Stapelt die Kinder vertikal */
-  align-items: center; /* Zentriert die Kinder horizontal */
-  justify-content: center; /* Zentriert die Kinder vertikal, wenn der Container mehr Höhe hat */
-  min-height: 100vh; /* Stellt sicher, dass der Container mindestens so hoch ist wie der Viewport */
+  justify-content:center;
+  align-items: center;
 }
-h3 {
-  margin: 40px 0 0;
-}
-.design-selector {
+
+.duplicated-images {
   display: flex;
-  align-items: center; 
+  flex-direction: column;
+ }
+
+.duplicated-images img {
+  margin-top: 0px; 
+  width: auto; /* Breite des Bildes */
+  height: auto; 
 }
-button {
-  margin: 0 5px;
+
+.duplicated-images img {
+  display: block; 
+  margin: 0px;
+  padding: 0px;
+  width: auto;
+  height: auto;
 }
-img {
-  width: 100px; /* oder die gewünschte Größe */
-  cursor: pointer;
+
+.images {
+  align-self: center;
+  margin: 0px; /* Stellt sicher, dass auch die Container keinen Außenabstand haben */
+  padding: 0px; /* Entfernt den Innenabstand vom Container */
 }
+
 </style>
