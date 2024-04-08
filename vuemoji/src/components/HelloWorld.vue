f04ebc495363b001b2cdef4eb951abeff884196d
pashaslayer <pavel.khakhlou@gmx.at>
2024-04-08 11:27:02 +0200

    pictures added, finished project

--- vuemoji/src/components/HelloWorld.vue
+++ vuemoji/src/components/HelloWorld.vue
@@ -1,62 +1,10 @@
 <template>
+  <body>
   <meta name="viewport" content="width=device-width, initial-scale=2">
-    <div class="container mt-5">
-      <div class="row justify-content-center">
-      <div
-        class="col-md-2 bg-info bg-opacity-25 border border-2 border-white rounded-start border-end align-items-center d-flex justify-content-center"
-      >
-        <label for="username" class="text-center">Username:</label>
-      </div>
-      <div
-        class="col-md-2 m-0 p-0 bg-white border border-2 border-white border-start-0 rounded-end"
-      >
-        <input
-          v-model="postData.username"
-          type="text"
-          class="form-control"
-          id="username-label"
-          @input="validateUsername"
-        />
-      </div>
-    </div>
+  <link rel="icon" href="@/assets/logo.png">
 
-    <div class="row justify-content-center">
-      <div
-        class="col-md-2 bg-info bg-opacity-25 border border-2 border-top-0 border-white rounded-start border-end align-items-center d-flex justify-content-center"
-      >
-        <label for="password" class="text-center">Password: </label>
-      </div>
-      <div
-        class="col-md-2 m-0 p-0 bg-white border border-2 border-white border-top-0 border-start-0 rounded-end"
-      >
-        <input
-          v-model="postData.password"
-          type="password"
-          class="form-control"
-          id="password-label"
-          aria-describedby="passwordHelp"
-          required
-          @input="validatePassword"
-        />
-      </div>
-    </div>
 
-    <div class="row">
-      <div
-        class="col-12 col-md-6 d-flex justify-content-md-end justify-content-center mb-2"
-      >
-        <button type="submit" class="btn btn-primary" @click="postLogin">
-          Bestätigen
-        </button>
-      </div>
-      <div
-        class="col-12 col-md-6 d-flex justify-content-md-start justify-content-center mb-2"
-      >
-        <button type="submit" class="btn btn-secondary">Abbrechen</button>
-      </div>
-    </div>
-  </div>
-
+  <img src="@/assets/logo.png" style="width: 150px">
 
   <div class="container">
     <div v-for="(designGroup, part) in designGroups" :key="part">
@@ -69,12 +17,15 @@
       <p>{{ designGroup.designs[designGroup.currentIndex].image }}</p>
     </div>
   </div>
-  <!-- Duplicated images outside of the loop -->
+
   <div class="duplicated-images">
     <div class="images" v-for="(designGroup, part) in designGroups" :key="'duplicated-' + part">
       <img :src="require('@/assets/' + designGroup.designs[designGroup.currentIndex].image)" />
     </div>
   </div>
+  <button @click="downloadImage">Bild herunterladen</button>
+  <canvas ref="canvas" style="display: none;"></canvas>
+</body>
 </template>
 
 
@@ -83,10 +34,6 @@ export default {
   name: 'vue-moji',
   data() {
     return {
-      postData: {
-        username: "",
-        password: "",
-      },
       designGroups: {
         head: {
           currentIndex: 0,
@@ -95,6 +42,13 @@ export default {
             { name: 'Hair 2', image: 'hair2.png' },
             { name: 'Hair 3', image: 'hair3.png' },
             { name: 'Hair 4', image: 'hair4.png' },
+            { name: 'Hair 5', image: 'hair5.png' },
+            { name: 'Hair 6', image: 'hair6.png' },
+            { name: 'Hair 7', image: 'hair7.png' },
+            { name: 'Hair 8', image: 'hair8.png' },
+            { name: 'Hair 9', image: 'hair9.png' },
+            { name: 'Hair 10', image: 'hair10.png' },
+
           ]
         },
         hair: {
@@ -104,6 +58,12 @@ export default {
             { name: 'Kopf 2', image: 'head2.png' },
             { name: 'Kopf 3', image: 'head3.png' },
             { name: 'Kopf 4', image: 'head4.png' },
+            { name: 'Kopf 5', image: 'head5.png' },
+            { name: 'Kopf 6', image: 'head6.png' },
+            { name: 'Kopf 7', image: 'head7.png' },
+            { name: 'Kopf 8', image: 'head8.png' },
+            { name: 'Kopf 9', image: 'head9.png' },
+            { name: 'Kopf 10', image: 'head10.png' },
           ]
         },
         body: {
@@ -116,6 +76,9 @@ export default {
             { name: 'Body 5', image: 'body5.png' },
             { name: 'Body 6', image: 'body6.png' },
             { name: 'Body 7', image: 'body7.png' },
+            { name: 'Body 8', image: 'body8.png' },
+            { name: 'Body 9', image: 'body9.png' },
+            { name: 'Body 10', image: 'body10.png' },
           ]
         },
         feet: {
@@ -126,6 +89,11 @@ export default {
             { name: 'Feet 3', image: 'feet3.png' },
             { name: 'Feet 4', image: 'feet4.png' },
             { name: 'Feet 5', image: 'feet5.png' },
+            { name: 'Feet 6', image: 'feet6.png' },
+            { name: 'Feet 7', image: 'feet7.png' },
+            { name: 'Feet 8', image: 'feet8.png' },
+            { name: 'Feet 9', image: 'feet9.png' },
+            { name: 'Feet 10', image: 'feet10.png' },
           ]
         }
       }
@@ -136,18 +104,62 @@ export default {
       let designGroup = this.designGroups[part];
       designGroup.currentIndex += direction;
 
-      // Überprüfen, ob der Index innerhalb der Grenzen des Arrays liegt
       if (designGroup.currentIndex >= designGroup.designs.length) {
-        designGroup.currentIndex = 0; // Zurück zum Anfang
+        designGroup.currentIndex = 0; 
       } else if (designGroup.currentIndex < 0) {
-        designGroup.currentIndex = designGroup.designs.length - 1; // Zum letzten Design
+        designGroup.currentIndex = designGroup.designs.length - 1; 
       }
-    }
+    },
+
+    drawImageOnCanvas() {
+  const canvas = this.$refs.canvas;
+  const ctx = canvas.getContext('2d');
+  canvas.width = 300; 
+  canvas.height = 500; 
+
+  const imagesToDraw = Object.values(this.designGroups).map(group => {
+    return require('@/assets/' + group.designs[group.currentIndex].image);
+  });
+
+  let accumulatedHeight = 0; 
+
+  const drawImage = (image, index) => {
+    const img = new Image();
+    img.onload = () => {
+      ctx.drawImage(img, (canvas.width - img.width) / 2, accumulatedHeight);
+      accumulatedHeight += img.height; 
+
+      if (index === imagesToDraw.length - 1) this.downloadCanvas(canvas);
+    };
+    img.src = image;
+  };
+
+  imagesToDraw.forEach(drawImage);
+},
+
+
+    downloadCanvas(canvas) {
+      const image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+      const link = document.createElement('a');
+      link.download = 'mein-maennchen.png';
+      link.href = image;
+      link.click();
+    },
+
+    downloadImage() {
+      this.drawImageOnCanvas();
+    },
+
   }
 }
 </script>
 
 <style scoped>
+body {
+  color: #01003D;
+  background-color: #01003D;
+}
+
 .container {
   display: flex;
   justify-content: center;
@@ -157,21 +169,23 @@ export default {
 .duplicated-images {
   display: flex;
   flex-direction: column;
-  align-items: center; /* This will center the children horizontally */
+  align-items: center; 
+  justify-content: center; 
+  background-color: whitesmoke;
+  width: 150px;
+  margin: 0 auto; 
 }
 
 .duplicated-images .images {
-  display: flex; /* Using flexbox for centering */
-  justify-content: center; /* This will center the images horizontally */
-  align-items: center; /* This will center the images vertically */
-  width: 100%; /* Ensures the container takes full width */
-  overflow: hidden; /* Hides the overflowing part of the images */
+  display: flex; 
+  justify-content: center; 
+  align-items: center;
+  width: 100%; 
+  overflow: hidden; 
 }
 
 .duplicated-images img {
-  max-width: 200%; /* Limits the image width to not exceed 200% of its container */
-  height: auto; /* Keeps the aspect ratio of the images */
+  max-width: 200%; 
+  height: auto; 
 }
-
-
 </style>
